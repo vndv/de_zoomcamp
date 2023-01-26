9d80213c28bef5beedbcae89df062054fcaa73c8
Ivan Matveev <imatveev@delimobil.ru>
2023-01-26 17:27:17 +0300

    uncomment creating bucket statment

--- week_1/homework_1.2/my-config.tf
+++ week_1/homework_1.2/my-config.tf
@@ -45,8 +45,8 @@ resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
 }
 
 // Creating a bucket using the key
-# resource "yandex_storage_bucket" "test" {
-#   access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
-#   secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
-#   bucket     = "de-zoomcamp"
-# }
+resource "yandex_storage_bucket" "test" {
+  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  bucket     = "de-zoomcamp"
+}
