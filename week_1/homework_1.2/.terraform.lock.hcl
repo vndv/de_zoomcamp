3e73f8718a6cb6a9531c851c6585162c71db5dfe
Ivan Matveev <imatveev@delimobil.ru>
2023-01-26 17:25:32 +0300

    complete terraform homework

---
+++ week_1/homework_1.2/.terraform.lock.hcl
@@ -0,0 +1,9 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/yandex-cloud/yandex" {
+  version = "0.84.0"
+  hashes = [
+    "h1:0qXDFW1FBkJT0S5X3rU+/dpbp2f8l3gp7cnB+sjh+AI=",
+  ]
+}
---
+++ week_1/homework_1.2/my-config.tf
@@ -0,0 +1,52 @@
+terraform {
+   required_providers {
+     yandex = {
+       source = "yandex-cloud/yandex"
+     }
+   }
+ }
+
+  variable "token" {
+     type = string
+ }
+
+ variable "cloud-id" {
+     type = string
+ }
+
+  variable "folder-id" {
+     type = string
+ }
+  
+ provider "yandex" {
+   token  =  var.token
+   cloud_id  = var.cloud-id
+   folder_id = var.folder-id
+   zone      = "ru-central1-a"
+ }
+
+
+ 
+resource "yandex_iam_service_account" "sa" {
+  name = "dtc"
+}
+
+// Assigning roles to the service account
+resource "yandex_resourcemanager_folder_iam_member" "sa-editor" {
+  folder_id = var.folder-id
+  role      = "storage.editor"
+  member    = "serviceAccount:${yandex_iam_service_account.sa.id}"
+}
+
+// Creating a static access key
+resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
+  service_account_id = yandex_iam_service_account.sa.id
+  description        = "static access key for object storage"
+}
+
+// Creating a bucket using the key
+# resource "yandex_storage_bucket" "test" {
+#   access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+#   secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+#   bucket     = "de-zoomcamp"
+# }
